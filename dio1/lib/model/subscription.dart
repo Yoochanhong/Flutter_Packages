165eb5445a1d208209eef59f3ce265a409208eca
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-29 23:34:42 +0900

    subscription 클래스 모델

---
+++ dio1/lib/model/subscription.dart
@@ -0,0 +1,24 @@
+class Subscription {
+  String? plan;
+  String? status;
+  String? paymentMethod;
+  String? term;
+
+  Subscription({this.plan, this.status, this.paymentMethod, this.term});
+
+  Subscription.fromJson(Map<String, dynamic> json) {
+    plan = json['plan'];
+    status = json['status'];
+    paymentMethod = json['payment_method'];
+    term = json['term'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data =<String, dynamic>{};
+    data['plan'] = plan;
+    data['status'] = status;
+    data['payment_method'] = paymentMethod;
+    data['term'] = term;
+    return data;
+  }
+}

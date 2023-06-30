daa564fde54ef2d2d0332bab3b4b7f282c7e61ff
yoochanhong <chanhongy6@naver.com>
2023-06-30 22:39:30 +0900

    flutter_secure_storage 패키지 사용해보기

---
+++ flutter_secure_storage/lib/success_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_secure_storage_package/main.dart';
+
+class SuccessPage extends StatefulWidget {
+  const SuccessPage({Key? key}) : super(key: key);
+
+  @override
+  State<SuccessPage> createState() => _SuccessPageState();
+}
+
+class _SuccessPageState extends State<SuccessPage> {
+  final storage = const FlutterSecureStorage();
+  dynamic userInfo = '';
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      userInfo = await storage.read(key: 'login');
+      if (userInfo == null) {
+        print('로그인 페이지로 이동');
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+            (route) => false);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                storage.delete(key: 'login');
+                storage.delete(key: 'accessToken');
+                storage.delete(key: 'refreshToken');
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                    (route) => false);
+              },
+              icon: const Icon(Icons.settings))
+        ],
+      ),
+      body: const Center(
+        child: Text('로그인 성공'),
+      ),
+    );
+  }
+}
